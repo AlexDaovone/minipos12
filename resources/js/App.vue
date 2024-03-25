e82ab43568784beeb31e5a7dc381db62707da380
AlexDaovone <alexshaopuvong@gmail.com>
2024-03-25 09:10:12 +0700

    update first project

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello VueJs3</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Minipos12App',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
